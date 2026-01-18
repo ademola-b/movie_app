5f0ea4519c0a0bdd1fc428def43f17801642924e
Faisol Ademola <bellofaisol@gmail.com>
2026-01-18 05:17:38 +0100

    Navigation Bar

---
+++ app/movies/[id].tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+const MovieDetails = () => {
+    return (
+        <View>
+            
+            <Text>MovieDetails</Text>
+        </View>
+    );
+}
